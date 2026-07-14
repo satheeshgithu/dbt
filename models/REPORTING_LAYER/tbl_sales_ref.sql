d301f5965df32fa1e8020b775e8aea99432b7fca
satheesh reddy <siddam.smd2633@gmail.com>
2026-07-14 07:03:09 +0000

    c2

---
+++ models/REPORTING_LAYER/tbl_sales_ref.sql
@@ -0,0 +1,38 @@
+with
+    customer as (
+ {{ref('stg_customer')}}
+    ),
+     orders as (
+{{ ref('stg_order') }}
+    ),
+    nation as (
+   {{ ref('stg_nation') }}
+    ),
+    region as (
+{{ ref('stg_region') }}
+    )
+
+select
+    c_custkey,
+    c_name,
+    c_address,
+    c_nationkey,
+    c_phone,
+    c_acctbal,
+    c_mktsegment,
+    c_comment,
+    o_orderkey,
+    o_custkey,
+    o_orderstatus,
+    o_totalprice,
+    o_orderdate,
+    o_orderpriority,
+    o_clerk,
+    o_shippriority,
+    o_comment,
+    nation,
+    region
+from customer c
+left join orders o on c.c_custkey = o.o_custkey
+left join nation n on c.c_nationkey = n.n_nationkey
+left join region r on n.n_regionkey = r.r_regionkey
